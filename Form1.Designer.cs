dd54c50c7f0c048f204a031b604a1ef44580af47
BlazeIT342 <yltrazavr@gmail.com>
2022-12-04 11:09:08 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace FractalJulia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(361, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 21);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(843, 472);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(854, 512);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Fractal by Mark Dmytriiev";
+            this.Text = "Fractal by Mark Dmytriiev";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FractalJulia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //функция зарисовки фрактала
+        public void DrawFractal(int w, int h, Graphics g, Pen pen) 
+        {
+            // при каждой итерации, вычисляется znew = zold² + С
+
+            // вещественная  и мнимая части постоянной C
+            double cRe, cIm;
+            // вещественная и мнимая части старой и новой
+            double newRe, newIm, oldRe, oldIm;
+            // Можно увеличивать и изменять положение
+            double zoom = 1, moveX = 0, moveY = 0;
+            //Определяем после какого числа итераций функция должна прекратить свою работу
+            int maxIterations = 300;
+
+            //выбираем несколько значений константы С, это определяет форму фрактала Жюлиа
+            cRe = -0.70176;
+            cIm = -0.3842;
+
+            //"перебираем" каждый пиксель
+            for (int x = 0; x < w; x++)
+                for (int y = 0; y < h; y++)
+                {
+                    //вычисляется реальная и мнимая части числа z
+                    //на основе расположения пикселей,масштабирования и значения позиции
+                    newRe = 1.5 * (x - w / 2) / (0.5 * zoom * w) + moveX;
+                    newIm = (y - h / 2) / (0.5 * zoom * h) + moveY;
+                    //i представляет собой число итераций 
+                    int i;
+
+                    //начинается процесс итерации
+                    for (i = 0; i < maxIterations; i++)
+                    {
+
+                        //Запоминаем значение предыдущей итерации
+                        oldRe = newRe;
+                        oldIm = newIm;
+
+                        // в текущей итерации вычисляются действительная и мнимая части 
+                        newRe = oldRe * oldRe - oldIm * oldIm + cRe;
+                        newIm = 2 * oldRe * oldIm + cIm;
+
+                        // если точка находится вне круга с радиусом 2 - прерываемся
+                        if ((newRe * newRe + newIm * newIm) > 4) break;
+                    }
+
+                    //определяем цвета
+                    //pen.Color = Color.FromArgb(255, (i * 9) % 255, 0, (i * 9) % 255);
+                    pen.Color = Color.FromArgb(255, (i * 50) % 255, 0, (i * 50) % 255);
+                    //рисуем пиксель
+                    g.DrawRectangle(pen, x, y, 1, 1);
+                }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Выбираем перо "myPen" черного цвета Black
+            //толщиной в 1 пиксель:
+            Pen myPen = new Pen(Color.Black, 1);
+            //Объявляем объект "g" класса Graphics и предоставляем
+            //ему возможность рисования на pictureBox1:
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+            //вызываем функцию рисования фрактала
+            DrawFractal(840, 620, g, myPen);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
